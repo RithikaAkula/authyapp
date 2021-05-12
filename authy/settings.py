da5f907297ece6e68ba3ab10802305bce2f6b14e
RithikaAkula <rithikaakula108@gmail.com>
2021-05-12 07:45:11 +0530

    deployed to heroku

--- authy/settings.py
+++ authy/settings.py
@@ -20,7 +20,7 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY_2')
+SECRET_KEY = os.environ.get('SECRET_KEY_A')
 
 # from django.core.exceptions import ImproperlyConfigured
 
